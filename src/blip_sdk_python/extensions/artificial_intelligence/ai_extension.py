978ada0abe2241d4356414d5256e5eb6ab172df6
louisbaggins <luis_araujo.c@icloud.com>
2021-05-31 11:46:57 -0300

    Add: Package versioning

--- src/blip_sdk_python/extensions/artificial_intelligence/ai_extension.py
+++ src/blip_sdk_python/extensions/artificial_intelligence/ai_extension.py
@@ -1,4 +1,6 @@
 from typing import Awaitable, List
+
+from lime_python.protocol.constants.content_types import ContentTypes
 from ..extension_base import ExtensionBase
 from .uri_templates import UriTemplates
 from .content_type import ContentType
@@ -40,7 +42,7 @@ class AiExtension(ExtensionBase):
             begin_date (str, optional): the begin date of the request.
             end_date (str, optional): the end date of the request.
             min_score (str, optional): minimal score.
-            max_score (str, optional): maximus score. 
+            max_score (str, optional): maximus score.
         """
 
         analysis_params = {
@@ -51,10 +53,10 @@ class AiExtension(ExtensionBase):
             'intents': intents,
             'feedbaccks': feedbacks,
             'source': source,
-            'begin_date': begin_date,
-            'end_date': end_date,
-            'min_score': min_score,
-            'max_score': max_score
+            'beginDate': begin_date,
+            'endDate': end_date,
+            'minScore': min_score,
+            'maxScore': max_score
         }
 
         query_params = {**analysis_params, **kwargs}
@@ -77,7 +79,7 @@ class AiExtension(ExtensionBase):
 
     async def set_analysis_by_email(
         self,
-        email_and_filter: str,
+        email_and_filter: dict,
         intents: list = [],
         feedbacks: list = [],
         source: str = None,
@@ -101,3 +103,116 @@ class AiExtension(ExtensionBase):
         Returns:
             Awaitable[Command]: Command response
         """
+        send_email_resource = {
+            'email': email_and_filter['email'],
+            'filter': self.build_resource_query(
+                UriTemplates.ANALYSIS,
+                {
+                    '$filter': email_and_filter['filter'],
+                    'intents': intents,
+                    'feedbacks': feedbacks,
+                    'source': source,
+                    'beginDate': begin_date,
+                    'endDate': end_date,
+                    'minScore': min_score,
+                    'maxScore': max_score
+                }
+            )
+        }
+        send_email_command = self.create_set_command(
+            UriTemplates.ANALYSIS_EMAIL,
+            send_email_resource,
+            ContentType.JSON_DOCUMENT
+        )
+
+        return await self.process_command_async(send_email_command)
+
+    async def set_analysis_feedback(self, id: str, analyses: dict):
+        analyses_feedback_resource = {
+            'item_type': ContentType.ANALYSIS_FEEDBACK,
+            'items': analyses
+        }
+        analyses_feedback_command = self.create_set_command(
+            self.build_uri(UriTemplates.ANALYSES_FEEDBACK, id=id),
+            analyses,
+            ContentType.ANALYSIS_FEEDBACK
+        )
+        return await self.process_command_async(analyses_feedback_command)
+
+    # analytics
+    async def get_analytics(self, id: str = None):
+        uri = self.build_uri(
+            UriTemplates.ANALYTICS_ID,
+            id) \
+            if id else UriTemplates.ANALYTICS
+
+        return await self.process_command_async(self.create_get_command(uri))
+
+    async def set_analytics(self, confusion_matrix):
+        confusion_matrix_resource = self.create_set_command(
+            UriTemplates.ANALYTICS,
+            ContentType.CONFUSION_MATRIX,
+            confusion_matrix
+        )
+
+        return await self.process_command_async(confusion_matrix_resource)
+
+    async def delete_analytics(self, id: str):
+        delete_analytics_command = self.create_delete_command(
+            self.build_uri(UriTemplates.ANALYTICS_ID, id)
+        )
+
+        self.process_command_async(delete_analytics_command)
+
+    # Intents
+
+    async def get_intent(self, id: str, deep: bool = False):
+        get_intent_command = self.create_get_command(
+            self.build_resource_query(
+                self.build_uri(UriTemplates.INTENTION, id),
+                {
+                    'deep': deep
+                }
+            )
+        )
+
+        return await self.process_command_async(get_intent_command)
+
+    async def get_intents(
+        self,
+        skip: int = 0,
+        take: int = 100,
+        deep: bool = False,
+        name: str = None,
+        ascending: bool = False
+    ) -> Awaitable[Command]:
+
+        intents_resource_query = self.build_resource_query(
+            UriTemplates.INTENTIONS,
+            {
+                '$skip': skip,
+                '$take': take,
+                'deep': deep,
+                'name': name,
+                '$ascending': ascending
+            }
+        )
+
+        return await self.process_command_async(
+            self.create_get_command(intents_resource_query)
+        )
+
+    async def set_intent(self, intent: dict) -> Awaitable[Command]:
+        return await self.process_command_async(
+            self.create_set_command(
+                UriTemplates.INTENTIONS,
+                intent,
+                ContentType.INTENTION
+            )
+        )
+
+    async def set_intents(self, intents: list) -> Awaitable[Command]:
+        set_intents_command = self.create_set_command(
+            UriTemplates.INTENTIONS,
+            ContentType
+        )
